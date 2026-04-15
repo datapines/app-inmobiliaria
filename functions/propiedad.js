c0652ac689cda6ad2c4ae238ee433a1b4daef5e8
datapines <datapines@gmail.com>
2026-04-15 14:03:37 -0500

    Update propiedad.js

--- functions/propiedad.js
+++ functions/propiedad.js
@@ -6,8 +6,8 @@ export async function onRequest(context) {
 
   if (!idBusqueda) return new Response("ID no proporcionado", { status: 400 });
 
-  const SHEET_URL = "https://docs.google.com/spreadsheets/d/1VctscCRyoQ-sdWa1vlGG0xsjjGY5Jznw6LaK20syz3g/export?format=csv&gid=0";
-  const URL_CONFIG = "https://docs.google.com/spreadsheets/d/1VctscCRyoQ-sdWa1vlGG0xsjjGY5Jznw6LaK20syz3g/export?format=csv&gid=563916861";
+  const SHEET_URL = "https://docs.google.com/spreadsheets/d/1RMACI9r5_GWIroZsDb68lPLodKwagXhgEwJX6t_zxHU/export?format=csv";
+  const URL_CONFIG = "https://docs.google.com/spreadsheets/d/1RMACI9r5_GWIroZsDb68lPLodKwagXhgEwJX6t_zxHU/export?format=csv&gid=2090640071";
 
   try {
     // ESTA ES LA PARTE QUE TE FALTABA: Pedir ambos al mismo tiempo
